01a891f19f0c0793b9f6ece07e8b1ed0b11f68e4
dudxo32 <37172704+dudxo32@users.noreply.github.com>
2022-08-31 08:35:09 +0900

    set naviagtion

---
+++ todoApp/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  todoApp
+//
+//  Created by 조영태 on 2022/08/27.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = UIColor.red
+    }
+}
--- todoApp/ViewController.swift
+++ todoApp/ViewController.swift
@@ -6,11 +6,32 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class ViewController: UIViewController {
 
+    let todoApp = UILabel().then {
+        $0.text = "TodoApp"
+        
+        $0.font = UIFont.boldSystemFont(ofSize: 30)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.addSubview(todoApp)
+        todoApp.snp.makeConstraints { make in
+            make.center.equalTo(view.snp.center)
+        }
+        
+        UIView.animate(withDuration: 2) {
+            self.todoApp.alpha = 0
+        } completion: { bool in
+            
+        }
+
+        
         // Do any additional setup after loading the view.
     }
 
